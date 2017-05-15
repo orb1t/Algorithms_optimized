0fc5924407400bb9bd8ead63bb3a857e8fe1d5ee
pritsheth <pritsheth99@gmail.com>
2017-05-15 15:14:29 +0530

    Updating Tree Directory with K distance Away Nodes

--- TreePrinter/src/KDistanceAway.java
+++ TreePrinter/src/KDistanceAway.java
@@ -3,6 +3,22 @@
  */
 public class KDistanceAway {
 
+    public void printkDistanceDown(TreeNode root, int k) {
 
+        if (root == null || k < 0)
+            return;
+
+        if (k == 0)
+            System.out.println(root.data);
+
+
+        printkDistanceDown(root.left,k-1);
+        printkDistanceDown(root.right,k-1);
+
+    }
+
+    public void printkDistanceUp(TreeNode root){
+
+    }
 
 }
