762c1fbacd8e8243938c90a97ec661a41b522904
pritsheth <pritsheth99@gmail.com>
2017-08-19 04:41:10 +0530

    Adding palindrom checker for linkedlist

--- LinkedList/src/CycleDetection.java
+++ LinkedList/src/CycleDetection.java
@@ -8,7 +8,7 @@ public class CycleDetection {
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null && fast.next != null && fast.next.next != null) {
+        while (slow != null && fast != null && fast.next != null) {
 
             slow = slow.next;
             fast = fast.next.next;
---
+++ LinkedList/src/PalindromeChecker.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jerry on 17-08-2017.
+ */
+public class PalindromeChecker {
+
+
+    
+
+}
